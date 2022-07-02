112315665349ee222061c26ea4de2f106b4816ca
Asif <aK788955@gmail.com>
2022-07-03 00:44:09 +0530

    new branch pull request

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const { Port } = require('./config/config');
 
 app.listen(Port, () => {
-  console.log(`Server running on Port ${Port}`);
+  console.log(`Server running on  ${Port}`);
 });
